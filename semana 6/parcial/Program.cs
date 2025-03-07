0e576627fdcc8beadc0f44e16a326495e53edca8
LNESCOBARP <LNESCOBARP@correo.url.edu.gt>
2025-03-07 05:02:33 -0800

    Add files via upload

---
+++ semana 6/parcial/Program.cs
@@ -0,0 +1,24 @@
+﻿class semana6
+{
+    public static void Main(string[] args)
+    {
+        double x,y;
+
+        Console.WriteLine("Escriba 1 número: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Escriba otro número: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Escriba otra vez un número: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        if
+        (a>b)
+       if (a > c)
+       if (b>c)
+       if (b>a)
+       
+            int.Parse ("mayor"+ a + "menor"+ b);
+        Console.WriteLine("mayor"+ a + "menor"+ b);
+       }
+       }
+       
