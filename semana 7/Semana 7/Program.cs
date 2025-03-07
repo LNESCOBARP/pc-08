e5c84db62e60293cee3243336637d32ae2172cae
LNESCOBARP <LNESCOBARP@correo.url.edu.gt>
2025-03-07 05:03:22 -0800

    Add files via upload

---
+++ semana 7/Semana 7/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class semana7_Nicolle
+{
+    static void Main()
+    {
+         string Nombre = "Nicolle Escobar";
+        Console.WriteLine(!Nombre.Contains("Nicolle"));
+    }
+}
